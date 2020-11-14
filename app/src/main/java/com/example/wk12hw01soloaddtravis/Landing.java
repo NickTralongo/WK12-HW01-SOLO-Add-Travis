ff65ced5a3d1c726b217acf78d4abe2ea4b36ece
bsuttonwills <38510991+bsuttonwills@users.noreply.github.com>
2020-11-13 18:21:02 -0800

    Initial commit

---
+++ app/src/main/java/com/example/wk12hw01soloaddtravis/Landing.java
@@ -0,0 +1,14 @@
+package com.example.wk12hw01soloaddtravis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Landing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
