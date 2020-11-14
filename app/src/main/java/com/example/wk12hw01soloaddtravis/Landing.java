9dcb33e59edc2374a3188560714f30e0424a43f3
bsuttonwills <38510991+bsuttonwills@users.noreply.github.com>
2020-11-13 18:43:18 -0800

    Added back button on landing page

--- app/src/main/java/com/example/wk12hw01soloaddtravis/Landing.java
+++ app/src/main/java/com/example/wk12hw01soloaddtravis/Landing.java
@@ -2,13 +2,25 @@ package com.example.wk12hw01soloaddtravis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Landing extends AppCompatActivity {
-
+    Button backBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        backBtn = (Button)findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Landing.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
